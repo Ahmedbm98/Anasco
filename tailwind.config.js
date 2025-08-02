242a7ae1c334cadd010f83d05a1faa2ebdff33df
Ahmedbm <ahmedabasset936@gmail.com>
2025-08-02 18:14:28 +0300

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "1000px",
+        xl: "1080px",
+        "2xl": "1536px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#9D6CFF",
+        redColor: "#FE365D",
+        secondary: "#4E008A",
+        bgMainColor: "#00758A",
+        bgGrayColor: "#F5F5F5",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
